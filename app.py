fd2d6c3f718799a4afc36f6eefdc58456e3b006f
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-03 15:48:40 -0400

    added another funtion to retrieve chunks of text from the pdf

--- app.py
+++ app.py
@@ -12,8 +12,16 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
     
-
-
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n", 
+        chunk_size=1000,
+        chunk_overlap=200, 
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+    
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDF's", page_icon=":books")
@@ -29,7 +37,7 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
                 
                 text_chunks = get_text_chunks(raw_text)
-        
+                st.write(text_chunks)
 
 
 if __name__ == '__main__':
