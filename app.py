43e62873361187cfc34c04303c3d35be83abfd2b
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-03 16:19:02 -0400

    vector store created

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 from pypdf import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+
 
 
 def get_pdf_text(pdf_docs):
@@ -21,6 +24,12 @@ def get_text_chunks(text):
     )
     chunks = text_splitter.split_text(text)
     return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+    
     
 def main():
     load_dotenv()
@@ -37,7 +46,9 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
                 
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
+                
+                vectorestore = get_vectorstore(text_chunks)
+
 
 
 if __name__ == '__main__':
