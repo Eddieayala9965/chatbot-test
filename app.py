1b0773b6bfd75654bdb86c543614edeafd6825fc
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-03 15:38:11 -0400

    coming along nicely, added a function that reads pdf text, essentially extracts it, its passed into our main, its pretty cool

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from dotenv import load_dotenv, find_dotenv
+from pypdf import PdfReader
+from langchain_text_splitters import CharacterTextSplitter
 
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+    
 
 
 def main():
@@ -12,8 +23,12 @@ def main():
     
     with st.sidebar:
         st.subheader("your documents")
-        st.file_uploader("Upload you PDF's here and click on process")
-        st.button("process")
+        pdf_docs = st.file_uploader("Upload you PDF's here and click on process", accept_multiple_files=True)
+        if st.button("process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                
+                text_chunks = get_text_chunks(raw_text)
         
 
 
